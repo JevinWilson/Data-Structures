32d680b2ba99c71ab1640e7db9cf5d27b75063d9
JevinWilson <jaw062701@gmail.com>
2024-02-14 21:41:27 -0500

    started #4 in lab instructions

--- Lab4/ssuds/src/array_list_utility_tests.cpp
+++ Lab4/ssuds/src/array_list_utility_tests.cpp
@@ -7,17 +7,84 @@ protected:
     ssuds::ArrayList<int> list;
 };
 
-TEST_F(ArrayListUtilityTest, Quicksort) {
+TEST_F(ArrayListUtilityTest, QuicksortTest) {
     list.append(4);
     list.append(1);
     list.append(3);
     list.append(2);
 
+    // Expected sorted list
+    ssuds::ArrayList<int> sorted_list;
+    sorted_list.append(1);
+    sorted_list.append(2);
+    sorted_list.append(3);
+    sorted_list.append(4);
+
     int left_index = 0;
     int right_index = list.size() - 1;
     ssuds::ArrayListUtility::quicksort(list, left_index, right_index, ssuds::sortOrder::Ascending);
 
-    for (int i = 0; i < list.size() - 1; i++) {
-        EXPECT_LE(list[i], list[i + 1]);
+    // Check that each element in the sorted list is equal to the expected list
+    for (int i = 0; i < list.size(); i++) {
+        EXPECT_EQ(sorted_list[i], list[i]);
     }
 }
+
+TEST_F(ArrayListUtilityTest, bubble_sortTest) {
+    list.append(4);
+    list.append(1);
+    list.append(3);
+    list.append(2);
+
+    // Expected sorted list
+    ssuds::ArrayList<int> sorted_list;
+    sorted_list.append(1);
+    sorted_list.append(2);
+    sorted_list.append(3);
+    sorted_list.append(4);
+
+    ssuds::ArrayListUtility::bubble_sort(list, ssuds::sortOrder::Ascending);
+
+    for (size_t i = 0; i < list.size() - 1; i++) {
+        EXPECT_EQ(sorted_list[i], list[i]);
+    }
+}
+
+// not working
+/*TEST_F(ArrayListUtilityTest, shuffleTest) {
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+
+    // save list for comparison later
+    ssuds::ArrayList<int> original_list = list;
+
+    bool shuffled = false;
+    for (int i = 0; i < 10; i++) {
+        ssuds::ArrayListUtility::shuffle(list);
+        // equal() = https://www.geeksforgeeks.org/stdequal-in-cpp/
+        if (!std::equal(list.begin(), list.end(), original_list.begin())) {
+            shuffled = true;
+            break;
+        }
+    }
+    EXPECT_TRUE(shuffled);
+}*/
+
+// not working
+/*TEST_F(ArrayListUtilityTest, binary_searchTest) {
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+
+    int left_index = 0;
+    int right_index = list.size() - 1;
+    ssuds::ArrayListUtility::quicksort(list, left_index, right_index, ssuds::sortOrder::Ascending);
+
+    int index = ssuds::ArrayListUtility::binary_search(list, 2, ssuds::sortOrder::Ascending);
+    EXPECT_EQ(1, index);
+}*/
--- Lab4/ssuds/src/ssuds.cpp
+++ Lab4/ssuds/src/ssuds.cpp
@@ -1,7 +1,15 @@
 // ssuds.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
+#ifndef MAIN_PROGRAM_1
+
 #include <iostream>
 #include <gtest/gtest.h>
+#include <array_list.h>
+#include <array_list_utility.h>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <random>
 
 
 int main()
@@ -10,3 +18,11 @@ int main()
     testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
+
+#else
+// MAIN_PROGRAM_2
+int main() {
+
+}
+
+#endif
