3159ffc823bbd60b639392cb8d8e676804d6a0dc
JevinWilson <jaw062701@gmail.com>
2024-03-08 23:19:59 -0500

    added functions to stack.h and queue.h

--- Lab6/lab06/ssuds/include/queue.h
+++ Lab6/lab06/ssuds/include/queue.h
@@ -1,19 +1,42 @@
 #pragma once
 
-#pragma once
-
 #include <linked_list.h>
 
 namespace ssuds 
 {
     template <class T>
-    class queue : private LinkedList<T> {
-        void enqueue(const T& new_val) {
-            this->append(new_val);
-            LinkedList<T>::append(new_val)
-        }
+    // is-a 
+    class queue : private LinkedList<T> 
+    {
     public:
+        void enqueue(const T& val) {
+            this->append(val);
+        }
+
+        T dequeue() {
+            if (this -> size() == 0)
+                throw std::out_of_range("out of range");
+            T val = (*this)[0];
+            this -> remove(0);
+            return val;
+        }
+
+        T head() const {
+            if (this -> size() == 0)
+                throw std::out_of_range("out of range");
+            return (*this)[0];
+        }
+
+        bool empty() {
+            return this -> size() == 0;
+        }
+
+        size_t size() const {
+            return LinkedList<T>::size();
+        }
+
         using LinkedList<T>::begin;
+        using LinkedList<T>::end;
     };
 }
 
--- Lab6/lab06/ssuds/include/stack.h
+++ Lab6/lab06/ssuds/include/stack.h
@@ -5,13 +5,36 @@
 namespace ssuds 
 {
     template <class T>
-    class stack
+    // has-a 
+    class stack 
     {
     protected:
         LinkedList<T> mList;
-    private:
+    public:
         void push(const T& val) {
             mList.prepend(val);
+        }
+
+        T pop() {
+            if (mList.size() == 0)
+                throw std::out_of_range("Out of range");
+            T val = mList[0];
+            mList.remove(0);
+            return val;
+        }
+
+        T top() const {
+            if (mList.size() == 0) 
+                throw std::out_of_range("out of range");
+            return mList[0];
+        }
+
+        bool empty() const {
+            return mList.size() == 0;
+        }
+
+        size_t size() const {
+            return mList.size();
         }
     };
 }
