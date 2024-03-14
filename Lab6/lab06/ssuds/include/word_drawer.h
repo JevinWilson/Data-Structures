d180af9b5ad3e39ef8fadd137bcba90c78583a3d
JevinWilson <jaw062701@gmail.com>
2024-03-14 00:25:05 -0400

    added array_list.h

--- Lab6/lab06/ssuds/include/word_drawer.h
+++ Lab6/lab06/ssuds/include/word_drawer.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <linked_list.h>
+#include <stack.h>
+#include <queue.h>
+#include <text_circle.h>
+#include <fstream>
+#include <string>
+#include <random>
+
