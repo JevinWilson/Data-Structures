e08f1bf633ec8d9af8ede16a8b9cc917374245b9
JevinWilson <jaw062701@gmail.com>
2024-03-24 17:33:14 -0400

    added all methods and working gtests for them

--- Lab7/src/ssuds/ordered_setTests.cpp
+++ Lab7/src/ssuds/ordered_setTests.cpp
@@ -22,11 +22,22 @@ TEST(OrderedSetTest, TraversalTest) {
     std::vector<int> result = set.traversal();
     ASSERT_EQ(expected, result);
 }
-/* don't forget to implement this
+
 TEST(OrderedSetTest, RebalanceTest) {
     ssuds::OrderedSet<int> set;
+    set.insert(1);
+    set.insert(2);
+    set.insert(3);
+    set.insert(4);
+    set.insert(5);
+    
+    int height_before = set.get_height();
+    ASSERT_EQ(height_before, 5);
 
-}*/
+    set.rebalance();
+    int height_after = set.get_height();
+    ASSERT_TRUE(height_after <= 3);
+}
 
 TEST(OrderedSetTest, ClearTest) {
     ssuds::OrderedSet<int> set;
@@ -64,4 +75,15 @@ TEST(OrderedSetTest, Get_heightTest) {
     set.insert(4);
     set.rebalance();
     ASSERT_EQ(3, set.get_height());
+}
+
+TEST(OrderedSetTEst, EraseTest) {
+    ssuds::OrderedSet<int> set;
+    set.insert(1);
+    set.insert(2);
+    set.insert(3);
+    set.insert(4);
+    ASSERT_TRUE(set.erase(2));
+    ASSERT_FALSE(set.contains(2));
+    ASSERT_EQ(3, set.size());
 }
