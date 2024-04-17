f72fb54e495a7bfd4da9b787ab085d0b91602b0c
JevinWilson <jaw062701@gmail.com>
2024-04-17 13:05:05 -0400

    added lab10 to github

--- Lab10/src/main.cpp
+++ Lab10/src/main.cpp
@@ -3,7 +3,12 @@
 #include <gtest/gtest.h>
 #else
 #include <SFML/Graphics.hpp>
-#include <graph_creator.h>
+#include <visual_graph.h>
+#endif
+
+#if !DO_UNIT_TEST
+// some helper functions used in main
+
 #endif
 
 int main()
@@ -14,15 +19,12 @@ int main()
 #else
     sf::RenderWindow window(sf::VideoMode(1024, 768), "Graph Creator");
     
-    // Note the trick I did with static's requires this
-    misc::GraphCreator::create_keycode_to_name_dbase();
-
     sf::Font status_font, circle_font;
     if (!status_font.loadFromFile("../../../media/fonts/Courier_Prime/CourierPrime-Regular.ttf") ||
         !circle_font.loadFromFile("../../../media/fonts/Open_Sans/static/OpenSans-Regular.ttf"))
         std::cout << "Error loading fonts\n";
 
-    misc::GraphCreator GC(circle_font, status_font);
+    misc::VisualGraph GC(circle_font, "../../../media/output.txt");
 
 
 
@@ -31,7 +33,8 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed || GC.handle_event(event))
+            if (event.type == sf::Event::Closed || 
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
                 window.close();
         }
 
